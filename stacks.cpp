4c971173d965da2a8fc86d661b6b00e70d8527dd
shubhadeep13 <shubhadeepdasgupta2013@gmail.com>
2021-03-01 12:29:28 +0530

    stack commit

--- stacks.cpp
+++ stacks.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+template<typename T>
+class node{
+    T value;
+    node *next;
+    
+    node(T value){
+        this->value = value;
+    }
+};
+
+template<typename T>
+class stack{
+
+};
 
 int main(int argc, char const *argv[])
 {
