a62d2e09252c3b082f5928822a1e325265e9a36c
shubhadeep13 <shubhadeepdasgupta2013@gmail.com>
2021-03-01 18:48:40 +0530

    initial queue commit

---
+++ queues.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+template <typename T>
+class node{
+
+};
+
+template <typename T>
+class queue{
+
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
