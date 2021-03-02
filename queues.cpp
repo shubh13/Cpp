c78824cdca4337e2c2e3f4606e8e8d95095636cf
shubhadeep13 <shubhadeepdasgupta2013@gmail.com>
2021-03-02 14:11:40 +0530

    new queue commit

--- queues.cpp
+++ queues.cpp
@@ -4,12 +4,36 @@ using namespace std;
 
 template <typename T>
 class node{
+    public:
+    T value;
+    node *next;
+    node *previous;
 
+    node(T value){
+        this->value = value;
+    }
 };
 
 template <typename T>
 class queue{
+    private:
+    int size;
+    node<T> *head = NULL;
+    node<T> *tail = NULL;
 
+    public:
+    queue(){
+        this->size = 0;
+    }
+
+    void enqueue(T value){
+        if(this->head = NULL){
+            this->head = new node<T>(value);
+            this->tail = this->head;
+        }else{
+            
+        }
+    }
 };
 
 int main(int argc, char const *argv[])
