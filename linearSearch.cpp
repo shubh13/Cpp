e1a42bbade534724d4d8303ecda89ce25cab945d
shubhadeep13 <shubhadeepdasgupta2013@gmail.com>
2021-03-03 12:11:48 +0530

    final linear commit

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
     /* code */
+
+    int arr[100], size, search, index;
+    bool flag = false;
+    cin>>size;
+
+    for(index = 0; index < size; index++){
+        cin>>arr[index];
+    }
+
+    cin>>search;
+
+    for(index = 0; index < size; index++){
+        if(search == arr[index]){
+            flag = true;
+        }
+    }
+
+    if(flag){
+        cout<<"Found"<<endl;
+    }else{
+        cout<<"Not Found"<<endl;
+    }
+
     return 0;
 }
